7f3a8628eb662180cd219570ba7319960ff46081
Mythyndian <zawistowskimaciej1@gmail.com>
2019-05-11 17:58:33 +0200

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,75 +1,92 @@
-
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-// Zmienne globalne
-
-int n;                   // Liczba wierzchołków
-char ** A;               // Macierz sąsiedztwa
-bool * visited;          // Tablica odwiedzin
-
-// Rekurencyjna procedura przejścia w głąb
-//----------------------------------------
-void DFS(int v)
+struct listEl
 {
-  int i;
+	listEl* next;
+	int value;
+};
 
-  visited[v] = true;     // Zaznaczamy węzeł jako odwiedzony
-  cout << setw(3) << v;  // Przetwarzamy węzeł (wypisujemy jego numer)
+listEl** M; //macierz list
+bool* visited; //tablica przechowujaca informacje o tym czy dany wezel zostal odwiedzony
 
-// Rekurencyjnie odwiedzamy nieodwiedzonych sąsiadów
+void DFS(int k)
+{
+	listEl* S, * p, * r;
+	p = new listEl; 
+	p->value = k;
+	p->next = NULL;
+	S = p; //wierzcholek startowy trafia na stos
 
-  for(i = 0; i < n; i++)
-    if((A[v][i] == 1) && !visited[i]) DFS(i);
+	visited[k] = true;
+
+	while (S) //kiedy cos jest na stosie
+	{
+		k = S->value;							//odczytujemy wierzcholek ze stosu
+		S = S->next;							//usuwamy wierzcholek ze stosu
+		cout << k << endl;						//aktualnie przetwarzany wierzcholek
+	}
+
+	//przechodzimy przez liste sasiedztwa
+	for (p = M[k]; p; p = p->next)
+	{
+		if (!visited[p->value])
+		{
+			r = new listEl;
+			r->value = p->value;
+			r->next = S;
+			S = r;
+			visited[p->value] = true;
+		}
+	}
 }
-
-// **********************
-// *** PROGRAM GŁÓWNY ***
-// **********************
 
 int main()
 {
-  int m,i,j,v1,v2;
+	int n, n1, n2, m, i;
+	listEl* p, * r;
+	cout << "Podaj liczbe wierzcholkow" << endl;	// Czytamy liczbę wierzchołków i krawędzi
+	cin >> n;                
+	cout << "Podaj liczbe krawedzi: " << endl;
+	cin >> m;
+	listEl *[n]
+	visited = new bool[n];
 
-  cin >> n >> m;         // Czytamy liczbę wierzchołków i krawędzi
+	//wypelnienie macierzy pustymi listami
 
-  A = new char * [n];    // Tworzymy tablicę wskaźników
-  visited = new bool[n]; // Tworzymy tablicę odwiedzin
+	for (i = 0; i < n; i++)
+	{
+		M[i] = NULL;
+		visited[i] = false;
+	}
+	
+	//Definicje krawedzi
 
-  for(i = 0; i < n; i++)
-    A[i] = new char[n];  // Tworzymy wiersze
+	for (int i = 0; i < m; i++)
+	{
+		cin >> n1 >> n2;
+		p = new listEl;
+		p->value = n2;
+		p->next = M[n1];
+		M[n1] = p;
+	}
 
-  // Macierz wypełniamy zerami
+	DFS(0); //wywolanie przeszukiwania
+	
+	for (i = 0; i < n; i++)
+	{
+		p = M[i];
+		while (p)
+		{
+			r = p;
+			p = p->next;
+		}
+	}
 
-  for(i = 0; i < n; i++)
-  {
-    visited[i] = false;  // Zerujemy tablicę odwiedzin
-    for(j = 0; j < n; j++) A[i][j] = 0;
-  }
+	delete[] M;
+	delete[] visited;
 
-  // Odczytujemy kolejne definicje krawędzi
 
-  for(i = 0; i < m; i++)
-  {
-    cin >> v1 >> v2;    // Wierzchołek startowy i końcowy krawędzi
-    A[v1][v2] = 1;      // Krawędź v1->v2 obecna
-  }
-
-  cout << endl;
-
-  // Przechodzimy graf w głąb
-
-  DFS(0);
-
-  // Usuwamy macierz
-
-  for(i = 0; i < n; i++) delete A[i];
-  delete [] A;
-  delete [] visited;
-
-  cout << endl;
-
-  return 0;
-} 
+	return 0;
+}	
