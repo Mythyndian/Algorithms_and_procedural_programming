e1a3e84b10d2997cd8e4147510dfe3037be8c770
Mythyndian <zawistowskimaciej1@gmail.com>
2019-05-21 20:03:05 +0200

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+void swap(int* a, int* b);
+void BubbleSort(int T[], int n);
+using namespace std;
+
+int main()
+{
+	int T[] = { 9,8,7,6,5,4,3,2,1,0 };
+	cout << "T = { ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << T[i] << " ";
+	}
+	cout << "}\n";
+
+	BubbleSort(T, 10);
+
+	cout << "T = { ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << T[i] << " ";
+	}
+	cout << "}";
+
+	return 0;
+}
+
+void BubbleSort(int T[], int n)
+{
+	int it = 0;
+ 
+	while (it < n - 1)
+	{
+		for (int j = 0; j < n - 1; j++)
+		{
+			if (T[j] > T[j + 1])
+				swap(&T[j], &T[j + 1]);
+		}
+		it++;
+	}
+}
+
+void swap(int* a, int* b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
